36b0e95604c8a4df962e831b621f9d66a2ea394b
Souvik Sarkar <souviksarkar2k3@gmail.com>
2025-07-10 00:28:22 +0530

    Background Service Worker Message Listener

--- extension/entrypoints/background.ts
+++ extension/entrypoints/background.ts
@@ -1,3 +1,28 @@
+import type { RuntimeMessage, RuntimeParams, RuntimeResponse } from '@/types/messaging'
+
+const tasks: { [key: string]: (args: RuntimeParams) => Promise<any> } = {
+	'save-keys': saveKeys,
+	'get-keys': getKeys,
+	'save-user': saveUser,
+	'get-user': getUser,
+	'save-did': saveDocument,
+	'get-did': getDocument,
+	'update-did': updateDocument
+}
+
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
-});
+	console.log('Hello background!', { id: browser.runtime.id })
+	browser.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+		const { type, params } = message as RuntimeMessage
+		tasks[type](params)
+			.then(data => {
+				const response: RuntimeResponse = { success: true, data }
+				sendResponse(response)
+			})
+			.catch(error => {
+				const response: RuntimeResponse = { success: false, error }
+				sendResponse(response)
+			})
+		return true
+	})
+})
