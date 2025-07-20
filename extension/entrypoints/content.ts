74d3bd251626fabf1f48121b77a6e186b50330d2
Souvik Sarkar <souviksarkar2k3@gmail.com>
2025-07-20 14:46:24 +0530

    Mounted Extension Integrated Signin UI

--- extension/entrypoints/content.ts
+++ extension/entrypoints/content.ts
@@ -1,6 +1,29 @@
+import type { WindowMessage } from '@/types/messaging'
+import { mountDidSelector } from '@/components/content/DidAuthentication'
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
-  },
-});
+  matches: ['<all_urls>'],
+  cssInjectionMode: 'ui',
+  main(ctx) {
+    console.log('Hello content.')
+    window.addEventListener('message', ({ source: sender, data }) => {
+      try {
+        if (sender != window) return
+        const { source, type, challenge } = data as WindowMessage
+        if (source !== 'byoi-sdk' || type !== 'signin-request') return
+        let ui: globalThis.IntegratedContentScriptUi<unknown>
+        const closeHandler = () => ui?.remove()
+        ui = createIntegratedUi(ctx, {
+          position: 'modal',
+          anchor: 'body',
+          append: 'first',
+          onMount: mountDidSelector(challenge, closeHandler),
+          onRemove: root => root?.unmount()
+        })
+        ui.mount()
+      } catch (e) {
+        console.error(e)
+      }
+    })
+  }
+})
